f0a96c2ba8ce40c5c57c2ad8f1d3141e7c16d014
Dan <dpedersen84@gmail.com>
2018-09-11 22:17:44 -0500

    transitions added

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,13 +3,16 @@
     <div class="holder">
       <form @submit.prevent="addSkill">
         <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate=" 'min:5'" name="skill">
-        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+          <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+            <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+          </transition>
         <!-- <input type="checkbox" id="checkbox" v-model="checked"> -->
       </form>
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ index + 1 }}. {{ data.skill }}</li>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key='index'>{{ index + 1 }}. {{ data.skill }}</li>
+        </transition-group>
       </ul>
-
       <!-- <p v-if="skills.length >=3">You have at least 3 skills</p>
       <p v-else>You have less than 3 skills</p> -->
       <p>These are the skills that you possess.</p>
@@ -21,111 +24,129 @@
 </template>
 
 <script>
-export default {
-  name: 'Skills',
-  data() {
-    return {
-      skill: "",
-      skills: [
-        { "skill": "Vue.js" },
-        { "skill": "Developer" },
-        { "skill": "Rock, paper, scissors" }
-      ],
-      // checked: false,
-      // showAlert: true,
-      // showClass: true,
-      // alertObject: {
-      //   alert: true,
-      // }
-      // bgColor: 'red',
-      // bgWidth: '100%',
-      // bgHeight: '30px'
-    }
-  },
-  methods: {
-        addSkill() {
-          this.$validator.validateAll().then((result) => {
-            if(result) {
-              this.skills.push ({ skill: this.skill })
-              this.skill = "";
-            } else {
-              console.log('Not valid')
-            }
-          })
-          // console.log(this.checked)
-        }
+  export default {
+    name: 'Skills',
+    data() {
+      return {
+        skill: "",
+        skills: [
+          { "skill": "Vue.js" },
+          { "skill": "Developer" },
+          { "skill": "Rock, paper, scissors" }
+        ],
+        // checked: false,
+        // showAlert: true,
+        // showClass: true,
+        // alertObject: {
+        //   alert: true,
+        // }
+        // bgColor: 'red',
+        // bgWidth: '100%',
+        // bgHeight: '30px'
       }
-}
+    },
+    methods: {
+          addSkill() {
+            this.$validator.validateAll().then((result) => {
+              if(result) {
+                this.skills.push ({ skill: this.skill })
+                this.skill = "";
+              } else {
+                console.log('Not valid')
+              }
+            })
+            // console.log(this.checked)
+          }
+        }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* .alert {
-  background-color: yellow;
-  width: 100%;
-  height: 30px;
-}
-.another-class {
-  border: 5px solid black;
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-} */
-.holder {
-  background: #fff;
-}
-ul {
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-}
-
-ul li {
-  padding: 20px;
-  font-size: 1.3em;
-  background-color: #E0EDF4;
-  border-left: 5px solid #3EB3F6;
-  margin-bottom: 2px;
-  color: #3E5252;
-}
-p {
-  text-align:center;
-  padding: 30px 0;
-  color: gray;
-}
-.container {
-    box-shadow: 0px 0px 40px lightgray;
-}
-input {
-  width: calc(100% - 40px);
-  border: 0;
-  padding: 20px;
-  font-size: 1.3em;
-  background-color: #323333;
-  color: #687F7F;
-}
-.alert {
-    background: #fdf2ce;
-    font-weight: bold;
+  @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+  @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
+  /* .alert {
+    background-color: yellow;
+    width: 100%;
+    height: 30px;
+  }
+  .another-class {
+    border: 5px solid black;
+  }
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
     display: inline-block;
-    padding: 5px;
-    margin-top: -20px;
-}
-.alert-in-enter-active {
-  animation: bounce-in .5s;
-}
-.alert-in-leave-active {
-  animation: bounce-in .5s reverse;
-}
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  } */
+  .holder {
+    background: #fff;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+  .container {
+      box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+  .alert {
+      background: #fdf2ce;
+      font-weight: bold;
+      display: inline-block;
+      padding: 5px;
+      margin-top: -20px;
+  }
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  i {
+    float:right;
+    cursor:pointer;
+  }
 </style>
